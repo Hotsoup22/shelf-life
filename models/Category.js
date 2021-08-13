2f1a55930042bf2d2e8f1c7c49e2defe4b57bc67
Jeff Lynch <jlynch3@uw.edu>
2021-08-13 16:37:51 -0700

    first commit

--- models/Category.js
+++ models/Category.js
@@ -5,5 +5,28 @@ const sequelize = require('../config/connection.js');
 class Category extends Model {}
 
 Category.init(
+    {
+        // define columns
+        id:{
+          type:DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true,
     
-)
+        },
+        Category_name:{
+          type: DataTypes.STRING,
+          allowNull: false,
+        }
+        
+      },
+      {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'category_name',
+      }
+    );
+    
+    module.exports = Category;
