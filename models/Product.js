a187bc475c1d235cefc27717356efee2ac0c0efc
hotsoup42 <jose22gw@gmail.com>
2021-08-14 11:09:47 -0700

    created category_id
    slight adjustment to Catergory model

--- models/Product.js
+++ models/Product.js
@@ -10,16 +10,16 @@ Product.init({
     primaryKey: true,
     autoIncrement: true,
   },
-  Product_name: {
+  product_name: {
     type: DataTypes.STRING,
     allowNull: false,
   },
-  datePurchased: {
+  date_purchased: {
     type: DataTypes.DATE,
     allowNull: false,
     defaultValue: DataTypes.NOW,
   },
-  experationDate: {
+  experation_date: {
     type: DataTypes.STRING,
     allowNull: true,
   },
@@ -30,6 +30,13 @@ Product.init({
       key: "id",
     },
   },
+  category_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: "category",
+      key: "id",
+    },
+  }
 });
 
 module.exports = Product;
