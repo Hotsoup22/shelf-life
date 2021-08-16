e39773a348a2480eef976db7a431bea8f113fb32
hotsoup42 <jose22gw@gmail.com>
2021-08-16 15:49:24 -0700

    added .bulkcreate for Catergory
    commented out. 'for const produc ot of productData

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,24 +1,29 @@
 const sequelize = require('../config/connection');
-const { User, Product } = require('../models');
+const { User, Product , Category } = require('../models');
 
 const userData = require('./userData.json');
 const productData = require('./productData.json');
-
+const categoryData = require('./categoryData.json')
 //----------------------------------------------
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  const users = await User.bulkCreate(userData, {
+  const product = await Product.bulkCreate(productData, {
     individualHooks: true,
     returning: true,
   });
 
-  for (const product of productData) {
-    await Product.create({
-      ...product,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
-  }
+  const category = await Category.bulkCreate(categoryData,{
+    individualHooks:true, 
+    returning:true,
+  });
+
+  // for (const product of productData) {
+  //   await Product.create({
+  //     ...product,
+  //     user_id: users[Math.floor(Math.random() * users.length)].id,
+  //   });
+  // }
 
   process.exit(0);
 };
