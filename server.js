701eea7d632a76b2aeb25f8e0fe997fad70a9b30
William Crownover <williamcrownover1@gmail.com>
2021-08-16 19:04:07 -0700

    Setup for server.js

--- server.js
+++ server.js
@@ -1,41 +1,53 @@
+// Required modules
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+// Required files
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
-
 const sequelize = require('./config/connection');
+
+// Setup sessions
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+// Setup express and server port
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set up Handlebars.js engine with custom helpers
+// Direct handlebars to helper files
 const hbs = exphbs.create({ helpers });
 
+// Session parameters
 const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
+    // Does Secret need to be the same for all developers?
+    secret: process.env.SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
 
+// Use the session
 app.use(session(sess));
 
-// Inform Express.js on which template engine to use
+// Connect express and handlebars
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+// Setup express
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Setup static path to public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Use routes
 app.use(routes);
 
+// Sequelize and server listening
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+    app.listen(PORT, () => console.log(`Now listening - http://localhost:${PORT}`));
 });
