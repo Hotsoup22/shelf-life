a9fe760d6f1e0b9f6756e27c3178aa4eee0f06a5
William Crownover <williamcrownover1@gmail.com>
2021-08-16 18:54:10 -0700

    Fixed seed.js to include category data
    Fixed links to connections.js in model files

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,29 +1,21 @@
-const sequelize = require('../config/connection');
+const sequelize = require('../config/connections');
 const { User, Product , Category } = require('../models');
 
 const userData = require('./userData.json');
 const productData = require('./productData.json');
-const categoryData = require('./categoryData.json')
+const categoryData = require('./categoryData.json');
 //----------------------------------------------
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  const product = await Product.bulkCreate(productData, {
+  await Product.bulkCreate(productData);
+
+  await Category.bulkCreate(categoryData);
+
+  await User.bulkCreate(userData, {
     individualHooks: true,
-    returning: true,
+    returning: true
   });
-
-  const category = await Category.bulkCreate(categoryData,{
-    individualHooks:true, 
-    returning:true,
-  });
-
-  // for (const product of productData) {
-  //   await Product.create({
-  //     ...product,
-  //     user_id: users[Math.floor(Math.random() * users.length)].id,
-  //   });
-  // }
 
   process.exit(0);
 };
