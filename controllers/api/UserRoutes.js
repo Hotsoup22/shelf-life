80fe5b7e102a8b7eb67a331fda2860d4d5f9bdce
hotsoup42 <jose22gw@gmail.com>
2021-08-16 21:21:50 -0700

    created controllers/api/index.js
    userRoutes now includes model Products and

--- controllers/api/UserRoutes.js
+++ controllers/api/UserRoutes.js
@@ -1,10 +1,19 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Product, Category } = require('../../models');
 
 
 router.get('/', async (req, res) => {
     try {
         const userData = await User.findAll({
+            include: [
+                {
+                  model: Product,
+                  include:[
+                     { model: Category,
+                     },
+                  ]
+                },
+              ],
             attributes: { exclude: ['password'] },
             order: [['name', 'ASC']],
           });
