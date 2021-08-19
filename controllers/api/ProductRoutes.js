a81ca16ca1929e21b4fb7d86f61fe3af3f54d21c
Jeff Lynch <jlynch3@uw.edu>
2021-08-18 19:15:36 -0700

    product routes

--- controllers/api/ProductRoutes.js
+++ controllers/api/ProductRoutes.js
@@ -1,22 +1,38 @@
- const express = require('express');
+//  const express = require('express');
  const router = require("express").Router();
+ const withAuth = require('../../utils/auth')
+ const {Product} = require('../../models')
  
  
 
  //get Product data
  router.post('/', withAuth, async (req, res,) => {
-    res.status(200).json({
-        message: 'Get request to products'
-    })
+  try {
+    const newProduct = await Product.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newProduct);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+// Delete blogpost
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+      const ProductData = await Product.destroy({
+          where: {
+              id: req.params.id
+          }
       });
- router.post('/', withAuth, async (req, res,) => {
-    res.status(200).json({
-        message: 'Post request to products'
-    })
-      });
-router.delete('/product', withAuth, async (req, res) => {
-    
-})
+
+      res.status(200).json(ProductData);
+
+  } catch (err) {
+      res.status(400).json(err);
+  }
+});
   
 
 module.exports = router;
