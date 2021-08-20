fed7dc268af507f04a1d75398c5d8f127ebd10db
hotsoup42 <jose22gw@gmail.com>
2021-08-19 21:19:45 -0700

    created public/addItem.js
    add item.js is not fully working

--- public/js/addItem.js
+++ public/js/addItem.js
@@ -1,25 +1,29 @@
-// const blogpostFormSubmit = async (event) => {
-//     event.preventDefault();
+const addItem = async (event) => {
+    event.preventDefault();
 
-//     const title = document.querySelector('#title').value.trim();
-//     const content = document.querySelector('#content').value.trim();
+    const name = document.querySelector('#itemName').value.trim();
+    const category = document.querySelector('#itemCategory').value.trim();
+     const date_purchased = document.querySelector('#purchaseDate').value.trim();
+    // const  experation_date = document.querySelector('#expirationDate').value.trim();
 
-//     if (title && content) {
-//         const response = await fetch('/', {
-//             method: 'POST',
-//             body: JSON.stringify({ title, content }),
-//             headers: { 'Content-Type': 'application/json' },
-//         });
+    if (name && category  && date_purchased) {
+    // if (name && category && date_purchased &&  experation_date) {
+        const response = await fetch('/api/product', {
+            method: 'POST',
+            body: JSON.stringify({ name, category ,date_purchased}),
+            // body: JSON.stringify({ name, category, date_purchased , experation_date }),
+            headers: { 'Content-Type': 'application/json' },
+        });
 
-//         if (response.ok) {
-//             document.location.replace('/pantry');
-//         } else {
-//             alert('alert');
-//         }
-//     }
-// };
+        if (response.ok) {
+            document.location.replace('/pantry');
+        } else {
+            alert('alert');
+        }
+    }
+};
 
-// // Listen for the blogpost form submission
-// document
-//     .querySelector('.post-form')
-//     .addEventListener('submit', blogpostFormSubmit);
+// Listen for the blogpost form submission
+document
+    .querySelector('#addItemBtn')
+    .addEventListener('click', addItem)
--- public/js/login.js
+++ public/js/login.js
@@ -18,7 +18,7 @@ const loginFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace('/pantry');
+      document.location.replace('/addItems');
     } else {
       alert('Failed to log in');
     }
