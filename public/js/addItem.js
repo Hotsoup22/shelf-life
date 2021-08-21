2c0d9a5e2c33747bfdd136dda0e3660a7e84b934
hotsoup42 <jose22gw@gmail.com>
2021-08-21 10:35:20 -0700

    updated for git pull

--- public/js/addItem.js
+++ public/js/addItem.js
@@ -1,21 +1,25 @@
+
+
 window.onload = function() {
     document.querySelector(".loginForm").style.display = "none";
     document.querySelector(".signupForm").style.display = "none";
     // document.getElementById("#userName").textContent = "";
   };
+  import datepicker from 'js-datepicker'
 const addItem = async (event) => {
     console.log("addItembtn")
     event.preventDefault();
 
     const product_name = document.querySelector('#itemName').value.trim();
     const category_id = document.querySelector('#itemCategory').value.trim();
-     const date_purchased = document.querySelector('#purchaseDate').value.trim();
+     const date_purchased = datepicker("#purchaseDate", options);;
      const expiration_date = document.querySelector('#expirationDate').value.trim();
      console.log("name" + product_name);
      console.log("category" +  category_id);
      console.log("date_p" + date_purchased);
      console.log("exp date" + expiration_date);
-
+     
+ 
 
      if (product_name && category_id && date_purchased &&  expiration_date) {
         const response = await fetch('/api/product', {
