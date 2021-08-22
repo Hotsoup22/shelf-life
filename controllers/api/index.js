fa72339b4c14ecb223fb4388fa1ce91ad455c329
William Crownover <williamcrownover1@gmail.com>
2021-08-22 12:18:54 -0700

    Updating file names

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,13 +2,12 @@
 // Required modules
 const router = require('express').Router();
 // Required files
-const userRoutes = require('./userRoutes');
-const productRoutes = require('./productRoutes')
+const userRoutes = require('./usersRoutes');
+const productRoutes = require('./productsRoutes');
 
 // Routing
 router.use('/users', userRoutes);
 router.use('/product', productRoutes);
-
 
 // Export
 module.exports = router;
