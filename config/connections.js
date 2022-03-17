30bc1d0ca449def33d512581ba7871e830603cfc
Hotsoup22 <jose22gw@gmail.com>
2022-03-17 15:17:50 -0700

    lines 11-14 were added in hopes of helping jawsdb

--- config/connections.js
+++ config/connections.js
@@ -4,7 +4,15 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize
+  (
+    process.env.JAWSDB_URL,
+    {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+    }
+  );
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
