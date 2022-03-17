eb85647a119a426c9dd37dd4911e2adde8445de3
Hotsoup22 <jose22gw@gmail.com>
2022-03-17 15:20:26 -0700

    reverted the last commit

--- config/connections.js
+++ config/connections.js
@@ -4,15 +4,7 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize
-  (
-    process.env.JAWSDB_URL,
-    {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-    }
-  );
+  sequelize = new Sequelize(process.env.JAWSDB_URL,);
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
